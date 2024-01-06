e1ded8cf11475cd20b502dafd87649a8b4492743
Matan Carmeli <45543087+matancarmeli7@users.noreply.github.com>
2024-01-06 11:03:10 +0200

    Add login and register pages (#1)
    * init matan
    
    * change project to find_my_dog
    
    ---------
    
    Co-authored-by: Matan Carmeli <matan.carmeli@aisap.ai>

---
+++ app/src/main/java/com/idz/find_my_dog/RegisterFragment.kt
@@ -0,0 +1,84 @@
+package com.idz.find_my_dog
+
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.Navigation
+import java.util.Locale
+
+class RegisterFragment : Fragment() {
+    var takeImage: ActivityResultLauncher<Uri>? = null
+    var cameraLauncher: ActivityResultLauncher<Void?>? = null
+    var signIn: TextView? = null
+    var cameraImg: ImageView? = null
+    var userImg: ImageView? = null
+    var signUp: Button? = null
+    var email: EditText? = null
+    var password: EditText? = null
+    var confirmPassword: EditText? = null
+    var setImg = false
+    var userImageUri: Uri? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        val context = requireActivity()
+        signIn = view.findViewById(R.id.signin_btn)
+        cameraImg = view.findViewById(R.id.camera_img)
+        signUp = view.findViewById(R.id.signup_btn)
+        email = view.findViewById(R.id.email_txt)
+        password = view.findViewById(R.id.password_txt)
+        confirmPassword = view.findViewById(R.id.confirmpassword_txt)
+        userImg = view.findViewById(R.id.user_img)
+
+        signIn?.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_registrationFragment_to_loginFragment))
+
+        cameraImg?.setOnClickListener { v -> cameraLauncher!!.launch(null) }
+
+        signUp?.setOnClickListener(View.OnClickListener{
+            val emailString = email?.text.toString().lowercase(Locale.ROOT)
+            if (!CredValidation.isRegisteredEmailValid(context, emailString)){
+                return@OnClickListener
+            }
+
+            val passwordString = password?.text.toString()
+            val confirmPasswordString = confirmPassword?.text.toString()
+            if (!CredValidation.isNewPasswordValid(context, passwordString, confirmPasswordString)){
+                return@OnClickListener
+            }
+
+            if (!setImg) {
+                Utils.showToast(context, "You have to set an image")
+                return@OnClickListener
+            }
+            Navigation.findNavController(view).popBackStack()
+        })
+
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { result: Bitmap? ->
+            if (result != null) {
+                userImg?.setImageBitmap(result)
+                setImg = true
+            }
+        }
+
+        takeImage = registerForActivityResult(ActivityResultContracts.TakePicture()) { result: Boolean? ->
+            if (result == true) {
+                userImg?.setImageURI(userImageUri)
+            }
+        }
+        return view
+    }
+}
