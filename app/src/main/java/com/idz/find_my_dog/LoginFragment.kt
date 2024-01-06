216fac7b9db3f7b7202fee8bd6e01db3ee167f78
Matan Carmeli <45543087+matancarmeli7@users.noreply.github.com>
2024-01-06 11:03:10 +0200

    Add login and register pages (#1)
    * init matan
    
    * change project to find_my_dog
    
    ---------
    
    Co-authored-by: Matan Carmeli <matan.carmeli@aisap.ai>

---
+++ app/src/main/java/com/idz/find_my_dog/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.idz.find_my_dog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.Navigation
+
+class LoginFragment : Fragment() {
+    var signUp: Button? = null
+    var login: Button? = null
+    var email: EditText? = null
+    var password: EditText? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val context = requireActivity()
+        signUp = view.findViewById(R.id.signup)
+        login = view.findViewById(R.id.login)
+        email = view.findViewById(R.id.email_txt)
+        password = view.findViewById(R.id.password_txt)
+
+        signUp?.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_registrationFragment))
+        login?.setOnClickListener(View.OnClickListener{
+            val emailString = email?.text.toString()
+            val passwordString = password?.text.toString()
+            if (!CredValidation.isLoginCredValid(context, emailString, passwordString)){
+                return@OnClickListener
+            }
+
+        })
+
+        return view
+    }
+}
