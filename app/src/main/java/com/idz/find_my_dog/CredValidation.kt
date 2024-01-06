271a4d8b1bc6fa964a95cacd2943832dc6b9d661
Matan Carmeli <45543087+matancarmeli7@users.noreply.github.com>
2024-01-06 11:03:10 +0200

    Add login and register pages (#1)
    * init matan
    
    * change project to find_my_dog
    
    ---------
    
    Co-authored-by: Matan Carmeli <matan.carmeli@aisap.ai>

---
+++ app/src/main/java/com/idz/find_my_dog/CredValidation.kt
@@ -0,0 +1,52 @@
+package com.idz.find_my_dog
+
+import android.content.Context
+import java.util.regex.Pattern
+
+object CredValidation {
+    fun isRegisteredEmailValid(context: Context, userEmail: String): Boolean {
+        if (userEmail.isEmpty()) {
+            Utils.showToast(context, "Email is required")
+            return false
+        } else if (!Pattern.compile("^(.+)@(\\S+)$").matcher(userEmail).matches()) {
+            Utils.showToast(context, "Bad email type")
+            return false
+        }
+
+        return true
+    }
+
+    fun isNewPasswordValid(context: Context, password: String, confirmPassword: String): Boolean {
+        if (password != "") {
+            if (password == confirmPassword) {
+                if (Pattern.compile("^.{0,5}$").matcher(password).matches()) {
+                    Utils.showToast(context, "password is too short (at least 6)")
+                    return false
+                }
+            } else {
+                Utils.showToast(context, "Password and Confirm password are different")
+                return false
+            }
+        } else {
+            Utils.showToast(context, "Password is empty")
+            return false
+        }
+
+        return true
+    }
+
+    fun isLoginCredValid(context: Context, userEmail: String, password: String): Boolean {
+        if (userEmail == "" || password == "") {
+            Utils.showToast(context, "email and password are required")
+            return false
+        } else if (!Pattern.compile("^(.+)@(\\S+)$").matcher(userEmail).matches()) {
+            Utils.showToast(context, "not an email type")
+            return false
+        } else if (Pattern.compile("^.{0,5}$").matcher(password).matches()) {
+            Utils.showToast(context, "password is too short (at least 6)")
+            return false
+        }
+
+        return true
+    }
+}
---
+++ app/src/main/java/com/idz/find_my_dog/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.idz.find_my_dog
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    var navCtrl: NavController? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHostMain) as NavHost?
+        navCtrl = navHost!!.navController
+        setupActionBarWithNavController(this, navCtrl!!)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (!super.onOptionsItemSelected(item)) {
+            when (item.itemId) {
+                android.R.id.home -> navCtrl!!.navigateUp()
+                else -> onNavDestinationSelected(item, navCtrl!!)
+            }
+        } else {
+            return true
+        }
+        return false
+    }
+}
