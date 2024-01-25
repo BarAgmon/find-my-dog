deafc836eb2a33ff6514cf91aafd0f55859d8719
Bar Agmon <email@address.com>
2024-01-25 16:27:18 +0200

    Filter posts is wotking

--- app/src/main/java/com/idz/find_my_dog/Modules/Posts/PostsFragment.kt
+++ app/src/main/java/com/idz/find_my_dog/Modules/Posts/PostsFragment.kt
@@ -3,7 +3,6 @@
     import android.os.Bundle
     import android.text.Editable
     import android.text.TextWatcher
-    import android.util.Log
     import android.view.LayoutInflater
     import android.view.View
     import android.view.ViewGroup
@@ -24,17 +23,17 @@
 
     class PostsFragment : Fragment() {
 
-        var postsListRv: RecyclerView? = null
+        private var postsListRv: RecyclerView? = null
         lateinit var posts: List<Post>
-        var newPostButton : FloatingActionButton?  = null
-        var model: Model = Model.instance
+        private var newPostButton : FloatingActionButton?  = null
+        private var model: Model = Model.instance
         private val args : PostsFragmentArgs by navArgs()
         private var searchAutoComplete: AutoCompleteTextView? = null
         private var citiesAdapter: ArrayAdapter<String>? = null
-        private val cities = Locations.instance.locations
+        private lateinit var cities: List<String>
+        private var postRvAdapter: PostRvAdapter? = null
 
         interface OnPostClickListener {
-            fun onPostClicked(clickedPost: Post)
             fun onItemClick(position: Int)
         }
         override fun onCreateView(
@@ -42,15 +41,24 @@
             savedInstanceState: Bundle?
         ): View? {
             val view = inflater.inflate(R.layout.fragment_posts_list, container, false)
-            newPostButton = view?.findViewById(R.id.add_new_post)
-            postsListRv = view.findViewById(R.id.posts_list_rv)
-            postsListRv?.setHasFixedSize(true)
-            postsListRv?.layoutManager = LinearLayoutManager(context)
-
+            setupUI(view)
+            Locations.instance.fetchCities { cities ->
+                // We are updating the UI inside the setupCitiesSearchbarArray, which needed to
+                // be performed from the main tread. Due to that, we are running the fetchCities
+                // function from the main tread. If not - application will crash.
+                activity?.runOnUiThread {
+                    //checks if the fragment is currently added to its activity, ensuring that it's
+                    // safe to interact with the fragment's views.
+                    if (isAdded) {
+                        this.cities = cities
+                        setupCitiesSearchbarArray(view)
+                    }
+                }
+            }
             model.getAllPosts {
                 this.posts = it
-                val adapter = PostRvAdapter(posts)
-                adapter.listener = object : OnPostClickListener {
+                this.postRvAdapter = PostRvAdapter(posts)
+                this.postRvAdapter?.listener = object : OnPostClickListener {
 
                     override fun onItemClick(position: Int) {
                         val clickedPost = posts[position]
@@ -59,22 +67,24 @@
                             Navigation.findNavController(view).navigate(action)
                         }
                     }
-
-                    override fun onPostClicked(clickedPost: Post) {
-                        Log.i("TAG", "STUDENT $clickedPost")
-                    }
                 }
-
-                postsListRv?.adapter = adapter
-                adapter.notifyDataSetChanged()
+                postsListRv?.adapter = this.postRvAdapter
+                this.postRvAdapter?.updatePosts(posts)
             }
-            newPostButton?.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_postsFragment_to_newPostDialogFragment))
-            setupCitiesSearchbarArray(view, postsListRv?.adapter)
+
             return view
         }
-        private fun setupCitiesSearchbarArray(view: View, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?) {
-            this.searchAutoComplete = view.findViewById<AutoCompleteTextView>(R.id.searchAutoCompleteTextView)
 
+        private fun setupUI(view: View) {
+            newPostButton = view.findViewById(R.id.add_new_post)
+            newPostButton?.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_postsFragment_to_newPostDialogFragment))
+            postsListRv = view.findViewById(R.id.posts_list_rv)
+            postsListRv?.setHasFixedSize(true)
+            postsListRv?.layoutManager = LinearLayoutManager(context)
+        }
+
+        private fun setupCitiesSearchbarArray(view: View) {
+            this.searchAutoComplete = view.findViewById<AutoCompleteTextView>(R.id.searchAutoCompleteTextView)
 
             // Setup the adapter
             citiesAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.select_dialog_item, cities)
@@ -87,22 +97,29 @@
                     object: ModelFirebase.getPostsByLocationCallback{
                         override fun onFailure() {
                             posts = listOf()
-                            adapter?.notifyDataSetChanged()
+                            postRvAdapter?.updatePosts(posts)
                         }
 
                         override fun onSuccess(postsByLocation: List<Post>) {
                             posts = postsByLocation
-                            adapter?.notifyDataSetChanged()
+                            postRvAdapter?.updatePosts(posts)
                         }
                 })
             }
 
             this.searchAutoComplete?.addTextChangedListener(object : TextWatcher {
                 override fun afterTextChanged(typedString: Editable) {
-                    // Filter the adapter based on the input text
-                    citiesAdapter?.filter?.filter(typedString)
+                    if (typedString.isEmpty()) {
+                        // User cleared the search, fetch all posts again
+                        model.getAllPosts {
+                            posts = it
+                            postRvAdapter?.updatePosts(posts)
+                        }
+                    } else {
+                        // Filter the adapter based on the input text
+                        citiesAdapter?.filter?.filter(typedString)
+                    }
                 }
-
                 override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
                 override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
             })
