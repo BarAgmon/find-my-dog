48f7bbad89b778cca695216dd478a9fbf1c0192c
Bar Agmon <111876103+m23anon@users.noreply.github.com>
2024-02-24 19:39:49 +0200

    Added scroll option to register page

--- app/src/main/java/com/idz/find_my_dog/MainActivity.kt
+++ app/src/main/java/com/idz/find_my_dog/MainActivity.kt
@@ -2,21 +2,35 @@ package com.idz.find_my_dog
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     var navCtrl: NavController? = null
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Make navbar invisible until user logged in
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.visibility = View.GONE
         val navHost = supportFragmentManager.findFragmentById(R.id.navHostMain) as NavHost?
         navCtrl = navHost!!.navController
         setupActionBarWithNavController(this, navCtrl!!)
 
+        navCtrl!!.addOnDestinationChangedListener { _, destination, _ ->
+            // Show BottomNavigationView only in certain destinations
+            when (destination.id) {
+                R.id.loginFragment, R.id.registrationFragment -> bottomNavigationView.visibility = View.GONE
+                else -> bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
